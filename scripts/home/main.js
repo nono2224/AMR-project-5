b81019c4a72b2d737cfaa6918ceeb1edfc075d63
nono2224 <88888931+nono2224@users.noreply.github.com>
2022-05-07 18:50:33 +0900

    update: ヘッダーボタンのアニメーションの追加
    ヘッダーがオープン，クローズした際のアニメーションを追加

--- scripts/home/main.js
+++ scripts/home/main.js
@@ -6,10 +6,18 @@ function header_open_close() {
     if (header_open_close_check == 0) {
         $("header").css("left", "calc(40vw - 100vw)");
         $(".header-content").css("opacity", "1");
+        $(".header-button-content-border-one").css("opacity", "0");
+        $(".header-button-content-border-two-one").css("transform", "translate(-50%,-50%) rotateZ(45deg)");
+        $(".header-button-content-border-two-two").css("transform", "translate(-50%,-50%) rotateZ(-45deg)");
+        $(".header-button-content-border-three").css("opacity", "0");
         header_open_close_check = 1;
     } else {
         $("header").css("left", "calc(50px - 100vw)");
         $(".header-content").css("opacity", "0");
+        $(".header-button-content-border-one").css("opacity", "1");
+        $(".header-button-content-border-two-one").css("transform", "translate(-50%,-50%)");
+        $(".header-button-content-border-two-two").css("transform", "translate(-50%,-50%)");
+        $(".header-button-content-border-three").css("opacity", "1");
         header_open_close_check = 0;
     }
 }
